5027b19990354255384fc20ad1ac2d1ff85fface
alextsapin <alextsapin@yandex.ru>
2022-03-05 19:55:34 +0200

    Lesson 1 completed

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,31 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Message from './Message';
+import AlternativeMessage from './Message';
+import './style.css';
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    name: 'Artem',
+    message: 'npm start нажимал?',
+    time: '20:00',
 }
 
 function HW1() {
     return (
         <div>
-            <hr/>
-            homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
         </div>
     )
 }
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-function Message() {
+type propsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message(props: propsType) {
     return (
-        <div>
-
+        <div className="message">
+            <img className="message__avatar" src={props.avatar}/>
+            <div className="message__user">
+                <p className="message__name">{props.name}</p>
+                <div className="message__text">
+                    <p>{props.message}</p>
+                    <p className="message__time">{props.time}</p>
+                </div>
+            </div>
         </div>
     )
 }
