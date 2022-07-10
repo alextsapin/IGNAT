e61479b5f072ef35147323a6679971bc4ce0488f
alextsapin <alextsapin@yandex.ru>
2022-07-10 10:53:11 +0200

    Updates

---
+++ precache-manifest.51269f9d35d631eef89104732084ef7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28bb07fd8658bda9e26f5c0ba746afc",
+    "url": "/IGNAT/index.html"
+  },
+  {
+    "revision": "c1df6619170f3e58db6c",
+    "url": "/IGNAT/static/css/main.a8f92ad8.chunk.css"
+  },
+  {
+    "revision": "9a24134718af50ec4420",
+    "url": "/IGNAT/static/js/2.2d457bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IGNAT/static/js/2.2d457bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1df6619170f3e58db6c",
+    "url": "/IGNAT/static/js/main.905201fd.chunk.js"
+  },
+  {
+    "revision": "be6246614132fb4c8565",
+    "url": "/IGNAT/static/js/runtime-main.5729cfb8.js"
+  },
+  {
+    "revision": "97f0702431688db1865c116306759a6a",
+    "url": "/IGNAT/static/media/bg.97f07024.jpg"
+  }
+]);
